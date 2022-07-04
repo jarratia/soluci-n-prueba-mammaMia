9e6500d2c1c35ec09fc738869832e005a54bc7fd
jarratia <jarratia34@gmail.com>
2022-07-04 13:43:44 -0400

    agregando lógica para vista detalle.jsx

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -1,10 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import { Outlet, useParams } from 'react-router-dom';
+import PizzasContext from '../context/PizzasProvider';
 
 const Detalle = () => {
+  const { pizzas } = useContext(PizzasContext);
+  const { id } = useParams();
+
+  const obtenerDatos = () => {
+    console.log(id);
+    console.log(pizzas);
+  };
+
+  obtenerDatos();
+
   return (
     <div>
       <Outlet />
-      Detalle
+      Detalle de la pizza
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ const Home = () => {
     <>
       <Navbar />
       <Header />
-
       <PizzasList>
         <Card />
       </PizzasList>
